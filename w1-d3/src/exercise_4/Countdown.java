2b6a9201ef2e4e5efed640f2a88fbdbd827815d4
Massolinii <massimilianoesposito548@gmail.com>
2023-05-04 16:54:07 +0200

    3/4

---
+++ w1-d3/src/exercise_4/Countdown.java
@@ -0,0 +1,11 @@
+package exercise_4;
+
+public class Countdown {
+	
+	public static int Capodanno(int numero) {
+		 for (int i = numero; i > 0; i--) {
+			 System.out.println(i);
+	        }
+		 return 0;
+	}
+}
