d0595214c357935d8c839ef4513e325bb55b2434
Massolinii <massimilianoesposito548@gmail.com>
2023-05-05 17:36:02 +0200

    made repo

--- w1-d3/src/exercise_2/Main.java
+++ w1-d3/src/exercise_2/Main.java
@@ -5,10 +5,15 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		
+		double m = 9.997;
+		System.out.print((int)Math.round(m));
+		
 		Scanner n = new Scanner(System.in);
 		System.out.print("Inserisci un numero da 0 a 3: ");
 		int sceltaNumero = n.nextInt();
 		
 		System.out.println(zeroToThree.checkNumber(sceltaNumero));
+		
+		
 	}
 }
